4899b5cea963ac84740e56a8248ff7249921ec8f
IsabellaAriasCue <iarias579@cue.edu.co>
2026-05-22 18:22:05 -0500

    GREEN - R1

---
+++ app/producto.py
@@ -0,0 +1,11 @@
+class Producto:
+
+    def __init__(self, nombre, precio_base):
+
+        if precio_base <= 0:
+            raise ValueError(
+                "El precio debe ser mayor que cero"
+            )
+
+        self.nombre = nombre
+        self.precio_base = precio_base
